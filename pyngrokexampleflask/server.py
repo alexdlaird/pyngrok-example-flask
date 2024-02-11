3d1d461960126f5163815aec629f62ce3aadae07
Alex Laird <contact@alexlaird.com>
2024-02-11 15:05:00 -0600

    Migrate to f-strings where convenient.

--- pyngrokexampleflask/server.py
+++ pyngrokexampleflask/server.py
@@ -30,7 +30,7 @@ def create_app():
 
         # Open a ngrok tunnel to the dev server
         public_url = ngrok.connect(port).public_url
-        print(" * ngrok tunnel \"{}\" -> \"http://127.0.0.1:{}\"".format(public_url, port))
+        print(f" * ngrok tunnel \"{public_url}\" -> \"http://127.0.0.1:{port}\"")
 
         # Update any base URLs or webhooks to use the public ngrok URL
         app.config["BASE_URL"] = public_url
