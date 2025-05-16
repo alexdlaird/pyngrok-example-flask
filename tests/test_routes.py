04416e9b0ee19172e9616fb53403c73529374023
Alex Laird <contact@alexlaird.com>
2025-05-16 12:14:12 -0500

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -4,15 +4,19 @@ __license__ = "MIT"
 import os
 import unittest
 
+from pyngrok import conf, process
+
 
 @unittest.skipIf(not os.environ.get("NGROK_AUTHTOKEN"), "NGROK_AUTHTOKEN environment variable not set")
 def test_healthcheck(client):
     response = client.get("/healthcheck")
     assert b'{"server": "up"}' in response.data
     assert "ngrok" in client.application.config["BASE_URL"]
+    assert process.is_process_running(conf.get_default().ngrok_path)
 
 
 def test_healthcheck_no_ngrok(client_no_ngrok):
     response = client_no_ngrok.get("/healthcheck")
     assert b'{"server": "up"}' in response.data
     assert "ngrok" not in client_no_ngrok.application.config["BASE_URL"]
+    assert not process.is_process_running(conf.get_default().ngrok_path)
